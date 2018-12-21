e753aa1d59944fd27975e9ba43aae5c809de1b7c
viennaltran <lanvienhtran@gmail.com>
2018-12-20 16:56:36 -0800

    Completed initial server and client setup

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 
 class Test extends Component {
     async componentDidMount(){
-        const resp = axios.get('/api/test');
+        const resp = await axios.get('/api/test');
 
         console.log('Test Response:', resp);
     }
     render(){
         return(
             <h1>This is test component</h1>
-        )
+        );
     }
 }
 
