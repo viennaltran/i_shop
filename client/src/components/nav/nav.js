6f30a653259e65829a5a491b9b8e0b0e0f56e578
viennaltran <lanvienhtran@gmail.com>
2018-12-20 18:17:39 -0800

    Setup webpack for sass, setup initial account routes on client, started navbar

---
+++ client/src/components/nav/nav.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+
+class Nav extends Component {
+    state={
+        common: [
+        {
+           text:'Home',
+           to:'/'
+        },
+        {
+            text:'Shop',
+            to:'/products'
+        }
+    ],
+    auth:[
+        {
+            text:'Orders',
+            to:'/account/orders'
+        },
+        {
+            text:'Profile',
+            to:'/account'
+        }
+    ],
+    nonAuth:[
+        {
+            text:'Sign In',
+            to:'/account/sign-in'
+        },
+        {
+            text:'Sign Up',
+            to:'/account/sign-up'
+
+        }
+    ]
+
+    }
+    render(){
+        return (
+            <nav className="teal darken-3"></nav>
+        );
+    }
+}
+
+export default Nav;
